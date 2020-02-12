4d3caecf1e404d88cc1cbafae86a9021629dfe27
Zeineb Yahiaoui <60674654+zeineb0@users.noreply.github.com>
2020-02-12 15:33:17 +0100

    creation du projet

---
+++ src/com/esprit/utilities/DataSource.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.utilities;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
+/**
+ *
+ * @author ahmed
+ */
+public class DataSource {
+    
+    private static DataSource data;
+    private String url = "jdbc:mysql://localhost:3306/wedevdb";
+    private String username = "root";
+    private String pasword = "";
+    private Connection con;
+
+    private DataSource() {
+        try {
+            con = (Connection) DriverManager.getConnection(url, username, pasword);
+            System.out.println("Class DataSource : connexion etablie");
+
+        } catch (SQLException e) {
+
+            System.out.println("Class DataSource :  Erreur de Connexion ");
+            System.out.println(e);
+        }
+    }
+
+    public Connection getConnection() {
+        return con;
+
+    }
+
+    public static DataSource getInstance() {
+        if (data == null) {
+            data = new DataSource();
+        }
+        return data;
+    }
+    
+}
