955a8f6795052d9e6221fb7ae7ddc6b89aa4f0d7
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-02-12 16:58:57 +0100

    Update Entrepot.java

--- src/com/esprit/entities/Entrepot.java
+++ src/com/esprit/entities/Entrepot.java
@@ -10,6 +10,61 @@ package com.esprit.entities;
  * @author asus
  */
 public class Entrepot {
- int id;
- 
+    int id_entrepot;
+    String adresse_entrepot;
+    int num_fiscale;
+    int id_max;
+
+    public Entrepot(int id_entrepot, String adresse_entrepot, int num_fiscale, int id_max) {
+        this.id_entrepot = id_entrepot;
+        this.adresse_entrepot = adresse_entrepot;
+        this.num_fiscale = num_fiscale;
+        this.id_max = id_max;
+    }
+
+    public Entrepot(String adresse_entrepot, int num_fiscale, int id_max) {
+        this.adresse_entrepot = adresse_entrepot;
+        this.num_fiscale = num_fiscale;
+        this.id_max = id_max;
+    }
+
+    public int getId_entrepot() {
+        return id_entrepot;
+    }
+
+    public String getAdresse_entrepot() {
+        return adresse_entrepot;
+    }
+
+    public int getNum_fiscale() {
+        return num_fiscale;
+    }
+
+    public int getId_max() {
+        return id_max;
+    }
+
+    public void setId_entrepot(int id_entrepot) {
+        this.id_entrepot = id_entrepot;
+    }
+
+    public void setAdresse_entrepot(String adresse_entrepot) {
+        this.adresse_entrepot = adresse_entrepot;
+    }
+
+    public void setNum_fiscale(int num_fiscale) {
+        this.num_fiscale = num_fiscale;
+    }
+
+    public void setId_max(int id_max) {
+        this.id_max = id_max;
+    }
+
+    @Override
+    public String toString() {
+        return "Entrepot{" + "id_entrepot=" + id_entrepot + ", adresse_entrepot=" + adresse_entrepot + ", num_fiscale=" + num_fiscale + ", id_max=" + id_max + '}';
+    }
+
+   
+    
 }
