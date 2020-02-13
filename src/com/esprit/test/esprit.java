c20264d9ed09d5bdcd476aa574b1a5a644a03c82
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-02-13 22:44:54 +0100

    Update esprit.java

--- src/com/esprit/test/esprit.java
+++ src/com/esprit/test/esprit.java
@@ -11,43 +11,40 @@ import com.esprit.services.impl.ServiceEntrepot;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-//import javafx.application.Application;
-//import javafx.fxml.FXMLLoader;
-//import javafx.scene.Parent;
-//import javafx.scene.Scene;
-//import javafx.stage.Stage;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  *
  * @author Zeineb_yahiaoui
  */
-public class esprit /*extends Application*/ {
+public class esprit extends Application {
     
-//    @Override
-//    public void start(Stage stage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-//        
-//        Scene scene = new Scene(root);
-//        
-//        stage.setScene(scene);
-//        stage.show();
-//    }
-//
-//    /**
-//     * @param args the command line arguments
-//     */
-    public static void main(String[] args) {
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        
+        Scene scene = new Scene(root);
+        
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws SQLException {
 //        launch(args);
+        ServiceEntrepot ent = new ServiceEntrepot();
+        Entrepot e1 = new Entrepot("Tunis", 520, 522,"libre", "delice");
         
-        ServiceEntrepot entrepot = new ServiceEntrepot();
+        ent.ajouter(e1);
+        ent.delete(8);
+                
         
-         Entrepot e1 = new Entrepot("Delice", "Ariana", 15236,500000);
-        
-        try {
-            entrepot.ajouter(e1);
-        } catch (SQLException ex) {
-            Logger.getLogger(esprit.class.getName()).log(Level.SEVERE, null, ex);
-        }
         
     }
     
