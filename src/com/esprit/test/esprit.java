591b3e9326346a305f8c63d889750442e9a4cf93
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2020-02-13 12:56:48 +0100

    Update esprit.java

--- src/com/esprit/test/esprit.java
+++ src/com/esprit/test/esprit.java
@@ -5,6 +5,11 @@
  */
 package com.esprit.test;
 
+import com.esprit.entities.Utilisateur;
+import com.esprit.services.impl.UtilisateurImpl;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,23 +20,34 @@ import javafx.stage.Stage;
  *
  * @author Zeineb_yahiaoui
  */
-public class esprit extends Application {
+public class esprit /*extends Application*/ {
     
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+//        
+//        Scene scene = new Scene(root);
+//        
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//    /**
+//     * @param args the command line arguments
+//     */
     public static void main(String[] args) {
-        launch(args);
+//        launch(args);
+        
+        UtilisateurImpl serviceUser = new UtilisateurImpl();
+        
+        Utilisateur user1 = new Utilisateur(1, "07478897", "akacha", "farouk", "transporteur");
+        
+        try {
+            serviceUser.ajouter(user1);
+        } catch (SQLException ex) {
+            Logger.getLogger(esprit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
