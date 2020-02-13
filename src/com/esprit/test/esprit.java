e877d9b7d01453b7c4f76e5c9201f32d8f78cdba
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-02-13 12:39:07 +0100

    Update esprit.java

--- src/com/esprit/test/esprit.java
+++ src/com/esprit/test/esprit.java
@@ -1,3 +1,4 @@
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -5,6 +6,11 @@
  */
 package com.esprit.test;
 
+import com.esprit.entities.Entrepot;
+import com.esprit.services.impl.testImlp;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
@@ -15,23 +21,34 @@ import javafx.stage.Stage;
  *
  * @author Zeineb_yahiaoui
  */
-public class esprit extends Application {
+public class esprit /*extends Application*/ {
     
-    @Override
-    public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
-        
-        Scene scene = new Scene(root);
-        
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+//        
+//        Scene scene = new Scene(root);
+//        
+//        stage.setScene(scene);
+//        stage.show();
+//    }
+//
+//    /**
+//     * @param args the command line arguments
+//     */
     public static void main(String[] args) {
-        launch(args);
+//        launch(args);
+        
+         testImlp ent = new testImlp();
+        
+         Entrepot e1 = new Entrepot("Delice", "Ariana", 15236,500000);
+        
+        try {
+            ent.ajouter(e1);
+        } catch (SQLException ex) {
+            Logger.getLogger(esprit.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
     }
     
 }
