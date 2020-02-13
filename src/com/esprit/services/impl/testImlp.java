57bc5c7c10389f4e49dc4ba9bb154e01f913bca2
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-02-13 12:39:17 +0100

    Update testImlp.java

--- src/com/esprit/services/impl/testImlp.java
+++ src/com/esprit/services/impl/testImlp.java
@@ -5,50 +5,11 @@
  */
 package com.esprit.services.impl;
 
-import com.esprit.entities.Entrepot;
 import com.esprit.services.test;
-import com.esprit.utilities.DataSource;
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.Statement;
-import java.util.List;
 
 /**
  *
  * @author Zeineb_yahiaoui
  */
-public class testImlp implements test <Entrepot> {
-    
-    private Connection con;
-    private Statement ste;
-
-    public testImlp() {
-        con = DataSource.getInstance().getConnection();
-
+public class testImlp implements test  {
     }
-    
-
-    @Override
-    public void ajouter(Entrepot e) throws SQLException {
-        ste = con.createStatement();
-        //String requeteInsert = ;
-        //ste.executeUpdate(requeteInsert);
-    }
-
-    @Override
-    public boolean delete(Entrepot e) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public boolean update(Entrepot e) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    @Override
-    public List<Entrepot> readAll() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
-    }
-
-    
-}
