c9ed3e3c4d578ac04786a957ed8a32a3a315a430
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-02-13 12:40:35 +0100

    Update ServiceEntrepot.java

--- src/com/esprit/services/impl/ServiceEntrepot.java
+++ src/com/esprit/services/impl/ServiceEntrepot.java
@@ -9,7 +9,11 @@ package com.esprit.services.impl;
 
 import com.esprit.entities.Entrepot;
 import com.esprit.services.IService;
+import com.esprit.utilities.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.List;
 
 /**
@@ -18,9 +22,23 @@ import java.util.List;
  */
 public class ServiceEntrepot implements IService<Entrepot> {
 
+    private Connection con;
+    private Statement ste;
+    
+    public ServiceEntrepot() {
+    con = DataSource.getInstance().getConnection();
+
+    }
+
+    
     @Override
     public void ajouter(Entrepot e) throws SQLException {
-        
+    PreparedStatement pre=con.prepareStatement("INSERT INTO `entrepot` ( `nom_entrepot`, `adresse_entrepot`, `num_fiscale`, `quantite_max`) VALUES ( ?, ?, ? ,?);");
+    pre.setString(1, e.getNom_entrepot());
+    pre.setString(2, e.getAdresse_entrepot());
+    pre.setInt(3, e.getNum_fiscale());
+    pre.setInt(4, e.getQuantite_max());
+    pre.executeUpdate();
     }
 
     @Override
