3963dab9e6828655daf0cc357247fb1a2dbae57e
Zeineb Yahiaoui <60674654+zeineb0@users.noreply.github.com>
2020-02-13 08:39:50 +0100

    Implementation de l'interface IProduit

--- src/com/esprit/services/IProduitService.java
+++ src/com/esprit/services/IProduitService.java
@@ -5,10 +5,19 @@
  */
 package com.esprit.services;
 
+import com.esprit.entities.Categorie;
+import com.esprit.entities.Produit;
+import java.util.List;
+
 /**
  *
  * @author Zeineb_yahiaoui
  */
 public interface IProduitService {
-    
+     public int ajouterProduit(Produit produit);
+    public int supprimerProduit(Produit produit);
+    public Produit consulterProduit(Produit produit);
+    public Produit modiferProduit(Produit produit);
+    public List<Produit> listeProduit();
+    public List<Produit> listeProduitPourUneCategorie(Categorie categorie );
 }
---
+++ src/com/esprit/services/impl/ProduitController.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.services.impl;
+
+import com.esprit.entities.Categorie;
+import com.esprit.entities.Produit;
+import com.esprit.services.IProduitService;
+import java.util.List;
+
+/**
+ *
+ * @author Zeineb_yahiaoui
+ */
+public class ProduitController implements IProduitService{
+
+    @Override
+    public int ajouterProduit(Produit produit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public int supprimerProduit(Produit produit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Produit consulterProduit(Produit produit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Produit modiferProduit(Produit produit) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Produit> listeProduit() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Produit> listeProduitPourUneCategorie(Categorie categorie) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
