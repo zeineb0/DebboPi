d1739703b55f4c046874739fbf3ced340f489ead
Zeineb Yahiaoui <60674654+zeineb0@users.noreply.github.com>
2020-02-13 08:09:32 +0100

    Update Categorie.java

--- src/com/esprit/entities/Categorie.java
+++ src/com/esprit/entities/Categorie.java
@@ -5,10 +5,55 @@
  */
 package com.esprit.entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  *
  * @author Zeineb_yahiaoui
  */
 public class Categorie {
+    
+    private int id;
+    private String nom;
+    //une categorie peut avoire plusieurs produits
+    private List<Produit> listProduit =new ArrayList<>();
+
+    public Categorie(int id, String nom) {
+        this.id = id;
+        this.nom = nom;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public List<Produit> getListProduit() {
+        return listProduit;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setListProduit(List<Produit> listProduit) {
+        this.listProduit = listProduit;
+    }
+
+    @Override
+    public String toString() {
+        return "Categorie{" + "id=" + id + ", nom=" + nom + ", listProduit=" + listProduit + '}';
+    }
+    
+    
+    
     
 }
