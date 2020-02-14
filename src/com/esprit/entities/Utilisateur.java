b7ba737bdb7c3ef582ffd1c3d69d4c43ce4e2f41
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2020-02-14 03:51:04 +0100

    Update Utilisateur.java

--- src/com/esprit/entities/Utilisateur.java
+++ src/com/esprit/entities/Utilisateur.java
@@ -5,6 +5,8 @@
  */
 package com.esprit.entities;
 
+import java.sql.Date;
+
 /**
  *
  * @author Dell
@@ -12,15 +14,34 @@ package com.esprit.entities;
 public class Utilisateur {
     
     private int id;
-    
-    private String nom,prenom,type,cin;
+    private String nom,prenom;
+    private long cin;
+    private Date date;
+    private String role;
+    private long telephone;
+    private String email;
+    private String password;
+    private int disponibilite;
+    private int nombre_max_commande;
 
-    public Utilisateur(int id, String cin, String nom, String prenom, String type) {
+    public Utilisateur() {
+    }
+    
+    
+    
+
+    public Utilisateur(int id, String nom, String prenom, long cin, Date date, String role, long telephone, String email, String password, int disponibilite, int nombre_max_commande) {
         this.id = id;
-        this.cin = cin;
         this.nom = nom;
         this.prenom = prenom;
-        this.type = type;
+        this.cin = cin;
+        this.date = date;
+        this.role = role;
+        this.telephone = telephone;
+        this.email = email;
+        this.password = password;
+        this.disponibilite = disponibilite;
+        this.nombre_max_commande = nombre_max_commande;
     }
 
     public int getId() {
@@ -29,14 +50,6 @@ public class Utilisateur {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getCin() {
-        return cin;
-    }
-
-    public void setCin(String cin) {
-        this.cin = cin;
     }
 
     public String getNom() {
@@ -55,19 +68,76 @@ public class Utilisateur {
         this.prenom = prenom;
     }
 
-    public String getType() {
-        return type;
+    public long getCin() {
+        return cin;
     }
 
-    public void setType(String type) {
-        this.type = type;
+    public void setCin(long cin) {
+        this.cin = cin;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public long getTelephone() {
+        return telephone;
+    }
+
+    public void setTelephone(long telephone) {
+        this.telephone = telephone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public int getDisponibilite() {
+        return disponibilite;
+    }
+
+    public void setDisponibilite(int disponibilite) {
+        this.disponibilite = disponibilite;
+    }
+
+    public int getNombre_max_commande() {
+        return nombre_max_commande;
+    }
+
+    public void setNombre_max_commande(int nombre_max_commande) {
+        this.nombre_max_commande = nombre_max_commande;
     }
 
     @Override
     public String toString() {
-        return "Utilisateur{" + "id=" + id + ", cin=" + cin + ", nom=" + nom + ", prenom=" + prenom + ", type=" + type + '}';
+        return "Utilisateur{" + "id=" + id + ", nom=" + nom + ", prenom=" + prenom + ", cin=" + cin + ", date=" + date + ", role=" + role + ", telephone=" + telephone + ", email=" + email + ", password=" + password + ", disponibilite=" + disponibilite + ", nombre_max_commande=" + nombre_max_commande + '}';
     }
     
+
     
     
     
