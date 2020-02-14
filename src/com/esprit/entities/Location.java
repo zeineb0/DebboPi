25384814955f9aab06848a34e7ae08a10ac14245
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-02-14 01:11:35 +0100

    Update Location.java

--- src/com/esprit/entities/Location.java
+++ src/com/esprit/entities/Location.java
@@ -14,82 +14,68 @@ import java.sql.Date;
 public class Location {
     int id_location;
     Date date_location;
-    int prix_location;
+    double prix_location;
     int FK_id_entrepot;
-    int FK_id_client;
-    int FK_id_fournisseur;
+    int FK_id_user;
 
-    public Location(int id_location, Date date_location, int prix_location, int FK_id_entrepot, int FK_id_client, int FK_id_fournisseur) {
+    public Location(int id_location, Date date_location, double prix_location, int FK_id_entrepot, int FK_id_user) {
         this.id_location = id_location;
         this.date_location = date_location;
         this.prix_location = prix_location;
         this.FK_id_entrepot = FK_id_entrepot;
-        this.FK_id_client = FK_id_client;
-        this.FK_id_fournisseur = FK_id_fournisseur;
+        this.FK_id_user = FK_id_user;
     }
-    
-       public Location(Date date_location, int prix_location, int FK_id_entrepot, int FK_id_client, int FK_id_fournisseur) {
+    public Location(Date date_location, double prix_location, int FK_id_entrepot, int FK_id_user) {
         this.date_location = date_location;
         this.prix_location = prix_location;
         this.FK_id_entrepot = FK_id_entrepot;
-        this.FK_id_client = FK_id_client;
-        this.FK_id_fournisseur = FK_id_fournisseur;
+        this.FK_id_user = FK_id_user;
     }
 
     public int getId_location() {
         return id_location;
     }
 
-    public Date getDate_location() {
-        return date_location;
-    }
-
-    public int getPrix_location() {
-        return prix_location;
-    }
-
-    public int getFK_id_entrepot() {
-        return FK_id_entrepot;
-    }
-
-    public int getFK_id_client() {
-        return FK_id_client;
-    }
-
-    public int getFK_id_fournisseur() {
-        return FK_id_fournisseur;
-    }
-
     public void setId_location(int id_location) {
         this.id_location = id_location;
+    }
+
+    public Date getDate_location() {
+        return date_location;
     }
 
     public void setDate_location(Date date_location) {
         this.date_location = date_location;
     }
 
-    public void setPrix_location(int prix_location) {
+    public double getPrix_location() {
+        return prix_location;
+    }
+
+    public void setPrix_location(double prix_location) {
         this.prix_location = prix_location;
+    }
+
+    public int getFK_id_entrepot() {
+        return FK_id_entrepot;
     }
 
     public void setFK_id_entrepot(int FK_id_entrepot) {
         this.FK_id_entrepot = FK_id_entrepot;
     }
 
-    public void setFK_id_client(int FK_id_client) {
-        this.FK_id_client = FK_id_client;
+    public int getFK_id_user() {
+        return FK_id_user;
     }
 
-    public void setFK_id_fournisseur(int FK_id_fournisseur) {
-        this.FK_id_fournisseur = FK_id_fournisseur;
+    public void setFK_id_user(int FK_id_user) {
+        this.FK_id_user = FK_id_user;
     }
 
     @Override
     public String toString() {
-        return "Location{" + "id_location=" + id_location + ", date_location=" + date_location + ", prix_location=" + prix_location + ", FK_id_entrepot=" + FK_id_entrepot + ", FK_id_client=" + FK_id_client + ", FK_id_fournisseur=" + FK_id_fournisseur + '}';
+        return "Location{" + "id_location=" + id_location + ", date_location=" + date_location + ", prix_location=" + prix_location + ", FK_id_entrepot=" + FK_id_entrepot + ", FK_id_user=" + FK_id_user + '}';
     }
-    
-
     
     
 }
