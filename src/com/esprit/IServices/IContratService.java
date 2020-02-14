f41de830156a0523bcdb79884cf9db353285519b
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2020-02-14 04:06:33 +0100

    Create IContratService.java

---
+++ src/com/esprit/IServices/IContratService.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.IServices;
+
+import com.esprit.entities.Contrat;
+import com.esprit.entities.ContratDetail;
+import com.esprit.entities.Entrepot;
+import com.esprit.entities.Livraison;
+import com.esprit.entities.Utilisateur;
+import java.util.List;
+
+/**
+ *
+ * @author Dell
+ */
+public interface IContratService {
+    
+    public void ajouterContrat (Contrat contrat,Utilisateur user,Entrepot entrepot);
+    public void supprimerContrat (Contrat contrat);
+    public void modifierContrat(Contrat contrat);
+    public List<ContratDetail> afficherContrat();
+    public void modifierEtatLivraison(Livraison livraison,String nouveau_etat);
+    public List<Utilisateur> afficherTransporteurLibre();
+    public void accepterLivraison(Livraison livraison);
+    public void refuserLivraison(Livraison livraison);
+    public List<Livraison> afficherLivraison(Utilisateur u);
+    
+}
