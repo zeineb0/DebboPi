e27a210067d32da257acea7faa7622f63d22a073
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2020-02-14 03:51:10 +0100

    Update Entrepot.java

--- src/com/esprit/entities/Entrepot.java
+++ src/com/esprit/entities/Entrepot.java
@@ -10,6 +10,80 @@ package com.esprit.entities;
  * @author asus
  */
 public class Entrepot {
- int id;
+   private int id_entrepot;
+   private String adress,etat,entreprise;
+   private int num_fisc;
+   private long quantite_max;
+
+    public Entrepot() {
+    }
+
+   
+   
+    public Entrepot(int id_entrepot, String adress, String etat, String entreprise, int num_fisc, long quantite_max) {
+        this.id_entrepot = id_entrepot;
+        this.adress = adress;
+        this.etat = etat;
+        this.entreprise = entreprise;
+        this.num_fisc = num_fisc;
+        this.quantite_max = quantite_max;
+    }
+
+    public int getId_entrepot() {
+        return id_entrepot;
+    }
+
+    public void setId_entrepot(int id_entrepot) {
+        this.id_entrepot = id_entrepot;
+    }
+
+    public String getAdress() {
+        return adress;
+    }
+
+    public void setAdress(String adress) {
+        this.adress = adress;
+    }
+
+    public String getEtat() {
+        return etat;
+    }
+
+    public void setEtat(String etat) {
+        this.etat = etat;
+    }
+
+    public String getEntreprise() {
+        return entreprise;
+    }
+
+    public void setEntreprise(String entreprise) {
+        this.entreprise = entreprise;
+    }
+
+    public int getNum_fisc() {
+        return num_fisc;
+    }
+
+    public void setNum_fisc(int num_fisc) {
+        this.num_fisc = num_fisc;
+    }
+
+    public long getQuantite_max() {
+        return quantite_max;
+    }
+
+    public void setQuantite_max(long quantite_max) {
+        this.quantite_max = quantite_max;
+    }
+
+    @Override
+    public String toString() {
+        return "Entrepot{" + "id_entrepot=" + id_entrepot + ", adress=" + adress + ", etat=" + etat + ", entreprise=" + entreprise + ", num_fisc=" + num_fisc + ", quantite_max=" + quantite_max + '}';
+    }
+   
+   
+   
+   
  
 }
