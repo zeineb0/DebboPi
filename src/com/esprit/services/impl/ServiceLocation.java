054bf30db0b9b659d3f5d3dd3d1d77b2ff301e32
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-02-18 18:47:17 +0100

    Update ServiceLocation.java

--- src/com/esprit/services/impl/ServiceLocation.java
+++ src/com/esprit/services/impl/ServiceLocation.java
@@ -32,12 +32,13 @@ public class ServiceLocation implements IServiceLocation<Location>{
     @Override
     public void ajouter(Location l) throws SQLException {
     try{
-    PreparedStatement pre=con.prepareStatement("INSERT INTO `location` (`id_location`, `date_location`, `prix_location`, `FK_id_entrepot`, `FK_id_user`)  VALUES ( ?, ?, ?, ?, ?);");
+    PreparedStatement pre=con.prepareStatement("INSERT INTO `location` (`id_location`, `date_deb_location`,`date_fin_location`, `prix_location`, `FK_id_entrepot`, `FK_id_user`)  VALUES ( ?, ?, ?, ?, ?);");
     pre.setInt(1, l.getId_location());
-    pre.setDate(2, l.getDate_location());
-    pre.setDouble(3, l.getPrix_location());
-    pre.setInt(4, l.getFK_id_entrepot());
-    pre.setInt(5, l.getFK_id_user());
+    pre.setDate(2, l.getDate_deb_location());
+    pre.setDate(3, l.getDate_fin_location());
+    pre.setDouble(4, l.getPrix_location());
+    pre.setInt(5, l.getFK_id_entrepot());
+    pre.setInt(6, l.getFK_id_user());
     pre.executeUpdate(); 
     }
     catch(SQLException ex)
@@ -48,7 +49,7 @@ public class ServiceLocation implements IServiceLocation<Location>{
     @Override
     public void delete(int nb) throws SQLException {
     try{
-        PreparedStatement pre = con.prepareStatement("DELETE FROM `location` WHERE `id_location`= ?");
+       PreparedStatement pre = con.prepareStatement("DELETE FROM `location` WHERE `id_location`= ?");
        pre.setInt(1, nb);
        pre.executeUpdate();
        System.out.println( nb + " deleted.");
@@ -64,13 +65,14 @@ public class ServiceLocation implements IServiceLocation<Location>{
     @Override
     public void update(Location l) throws SQLException {
 try {
-            PreparedStatement pre=con.prepareStatement("UPDATE `location` SET `date_location`=?,`prix_location`=?,`FK_id_entrepot`=?,`FK_id_user`=? WHERE `id_location`=?;");
+            PreparedStatement pre=con.prepareStatement("UPDATE `location` SET `date_deb_location`=?,`date_fin_location`=?,`prix_location`=?,`FK_id_entrepot`=?,`FK_id_user`=? WHERE `id_location`=?;");
             pre.setInt(1, l.getId_location());
-            pre.setDate(2, l.getDate_location());
-            pre.setDouble(3, l.getPrix_location());
-            pre.setInt(4, l.getFK_id_entrepot());
-            pre.setInt(5, l.getFK_id_user());
-            pre.setInt(6, l.getId_location());
+            pre.setDate(2, l.getDate_deb_location());
+            pre.setDate(3, l.getDate_fin_location());
+            pre.setDouble(4, l.getPrix_location());
+            pre.setInt(5, l.getFK_id_entrepot());
+            pre.setInt(6, l.getFK_id_user());
+            pre.setInt(7, l.getId_location());
             pre.executeUpdate();
             System.out.println("la loc num"+ l.getId_location()+ " updated.");
             
@@ -86,15 +88,16 @@ try {
     ResultSet rs=ste.executeQuery("select * from location");
      while (rs.next()) {                
                int id_location=rs.getInt(1);
-               Date date_location = rs.getDate(2);
-               double prix_location = rs.getDouble(3);
-               int FK_id_entrepot = rs.getInt(4);
-               int FK_id_user = rs.getInt(5);
+               Date date_deb_location = rs.getDate(2);
+               Date date_fin_location = rs.getDate(3);
+               double prix_location = rs.getDouble(4);
+               int FK_id_entrepot = rs.getInt(5);
+               int FK_id_user = rs.getInt(6);
                
                
              
                
-    Location l = new Location(id_location, date_location, prix_location, FK_id_entrepot, FK_id_user);
+    Location l = new Location(id_location, date_deb_location, date_fin_location, prix_location, FK_id_entrepot, FK_id_user);
     locations.add(l);
      }
     return locations;
@@ -105,12 +108,13 @@ try {
    ResultSet rs = ste.executeQuery("SELECT * FROM location ORDER BY`prix_location`ASC");
    while (rs.next())
    {           int id_location=rs.getInt(1);
-               Date date_location = rs.getDate(2);
-               double prix_location = rs.getDouble(3);
-               int FK_id_entrepot = rs.getInt(4);
-               int FK_id_user = rs.getInt(5);
+               Date date_deb_location = rs.getDate(2);
+               Date date_fin_location = rs.getDate(3);
+               double prix_location = rs.getDouble(4);
+               int FK_id_entrepot = rs.getInt(5);
+               int FK_id_user = rs.getInt(6);
                
-    Location l = new Location(id_location, date_location, prix_location, FK_id_entrepot, FK_id_user);
+    Location l = new Location(id_location, date_deb_location, date_fin_location, prix_location, FK_id_entrepot, FK_id_user);
     locations2.add(l);
 }
      return locations2;  
@@ -122,12 +126,14 @@ public List<Location> trierLocationParDateCroissant() throws SQLException{
    ResultSet rs = ste.executeQuery("SELECT * FROM location ORDER BY`date_location`ASC");
    while (rs.next())
    {           int id_location=rs.getInt(1);
-               Date date_location = rs.getDate(2);
-               double prix_location = rs.getDouble(3);
-               int FK_id_entrepot = rs.getInt(4);
-               int FK_id_user = rs.getInt(5);
+              Date date_deb_location = rs.getDate(2);
+               Date date_fin_location = rs.getDate(3);
+               double prix_location = rs.getDouble(4);
+               int FK_id_entrepot = rs.getInt(5);
+               int FK_id_user = rs.getInt(6);
                
-    Location l = new Location(id_location, date_location, prix_location, FK_id_entrepot, FK_id_user);
+               
+    Location l = new Location(id_location, date_deb_location, date_fin_location, prix_location, FK_id_entrepot, FK_id_user);
     locations3.add(l);
 }
      return locations3;
