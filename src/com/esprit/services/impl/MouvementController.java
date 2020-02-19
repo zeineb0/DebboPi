44a0eb92e69d532718d1d85ea632d7b2123bc459
Zeineb Yahiaoui <60674654+zeineb0@users.noreply.github.com>
2020-02-19 13:49:54 +0100

    Update MouvementController.java

--- src/com/esprit/services/impl/MouvementController.java
+++ src/com/esprit/services/impl/MouvementController.java
@@ -22,9 +22,10 @@ import java.util.logging.Logger;
  */
 public class MouvementController implements IMouvementService{
 
-Connection conn = DataSource.getInstance().getConnection();
+    Connection conn = DataSource.getInstance().getConnection();
     private ResultSet rs;
     private PreparedStatement ps;
+    
     @Override
     public void ajouterMouvement(MouvementStock m) {
         String req = "INSERT INTO `mouvement_du_stock`( `nature_mouvement`, `date_mouv`, `FK_id_produit`, `FK_id_entrepot`) "
@@ -41,7 +42,16 @@ Connection conn = DataSource.getInstance().getConnection();
 
     @Override
     public void modifierMouvement(MouvementStock m) {
-     String req = "UPDATE `mouvement_du_stock` SET `nature_mouvement`=?,`date_mouv`=?,`FK_id_produit`=?,`FK_id_entrepot`=? WHERE 1";   
+     String req = "UPDATE `mouvement_du_stock` SET `nature_mouvement`=?,`date_mouv`=?,`FK_id_produit`=?,`FK_id_entrepot`=? WHERE 1";  
+     try{
+     ps=conn.prepareStatement(req);
+     ps.setString(1,m.getNatureDuStock());
+     ps.setDate(2,m.getDateMouv());
+     ps.setInt(3,m.getP().getId());
+     //ps.setString(4,m.);
+     }
+     catch(SQLException ex){
+             System.out.println("erreur");}
      
     }
     
@@ -56,7 +66,7 @@ Connection conn = DataSource.getInstance().getConnection();
         String req= "SELECT * FROM `mouvement_du_stock` WHERE `nature_mouvement` LIKE ?";
         try {
         ps=conn.prepareStatement(req);
-        ps.setString(1,m.getNatureDuStock());
+        ps.setString(1,nature);
         rs=ps.executeQuery();
         
          while (rs.next()) 
