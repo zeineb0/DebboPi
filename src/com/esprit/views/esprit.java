0446e2e0ca3223d53ad4e740e84ce8a4d90ca737
Zeineb Yahiaoui <60674654+zeineb0@users.noreply.github.com>
2020-02-19 13:48:55 +0100

    Update esprit.java

--- src/com/esprit/views/esprit.java
+++ src/com/esprit/views/esprit.java
@@ -36,31 +36,40 @@ public class esprit extends Application {
         //ajout 
 //        categorieController.ajouterCategorie(c1);
         
-        categorieController.consulterCategorie(c1);
+//        c1.setNom("infoooooooo");
+//        c1.setId(4);
+//        categorieController.modiferCategorie(c1);
+//                categorieController.consulterCategorie("infoooooooo");
+//                categorieController.supprimerCategorie("informatique1");
+                    List<Categorie> liste = categorieController.listCategorie();
         //c1.setId(2);
         //instance produit
+        
         ProduitController produitController=new ProduitController();
-        c1.setId(1);
+           
+       // c1.setId(1);
        Produit p1 = new Produit("lait", 1.2, 20,10 , false,c1);
+        produitController.consulterProduit("noooo");
+               produitController.supprimerProduit("lait");
 //        Produit p2 = new Produit("yaghort", 1.4, 20,10 , false,null);
 //        Produit p3 = new Produit("yt", 1.4, 20,10 , false,null);
         produitController.ajouterProduit(p1);
-        p1.setId(2);
-        produitController.supprimerProduit(p1);
+//        p1.setId(2);
+//        produitController.supprimerProduit(p1);
         
-//        Produit p1=new Produit();
+//        Produit p3=new Produit();
 //        p1.setNom("ordinateur");
 //        p1.setPrix(2000);
 //        p1.setPromotion(false);
 //        p1.setQuantite(100);
 //        p1.setReserve(4);
 //        p1.setCategorie(c1);
-        // modifier
-        
-        p1.setId(3);
-        p1.setNom("noooo");
-      produitController.modiferProduit(p1);
-////        produitController.ajouterProduit(p1);
+//        // modifier
+//        produitController.ajouterProduit(p3);
+//        p1.setId(3);
+//        p1.setNom("noooo");
+//      produitController.modiferProduit(p1);
+//////        produitController.ajouterProduit(p1);
 //
 //        //consulter
 //        p1.setId(3);
