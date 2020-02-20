bd064db43cfda2109779c9de8ff4ce8d792d67bf
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-02-20 22:22:40 +0100

    Update SupprimerEntrepotController.java

--- src/com/esprit/views/SupprimerEntrepotController.java
+++ src/com/esprit/views/SupprimerEntrepotController.java
@@ -8,13 +8,19 @@ package com.esprit.views;
 import com.esprit.entities.Entrepot;
 import com.esprit.services.impl.ServiceEntrepot;
 import com.esprit.utilities.DataSource;
+import java.io.IOException;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -73,6 +79,43 @@ public class SupprimerEntrepotController implements Initializable {
     quanmax.setText(ma);
     
     
+    }
+
+    @FXML
+    private void onClick(ActionEvent event) throws IOException {
+         Parent liste = FXMLLoader.load(getClass().getResource("ajouterEntrepot.fxml"));
+
+           Scene ListeE = new Scene(liste);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(ListeE);
+        window.show();
+    }  
+    @FXML
+    private void onClick1(ActionEvent event) throws IOException {
+         
+        Parent liste = FXMLLoader.load(getClass().getResource("supprimerEntrepot.fxml"));
+           Scene ListeE = new Scene(liste);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(ListeE);
+        window.show();
+        
+    }   
+    @FXML
+    private void onClick2(ActionEvent event) throws IOException {
+        Parent liste = FXMLLoader.load(getClass().getResource("modifierEntrepot.fxml"));
+          Scene ListeE = new Scene(liste);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(ListeE);
+        window.show();
+    }
+
+    @FXML
+    private void onClick3(ActionEvent event) throws IOException {
+        Parent liste = FXMLLoader.load(getClass().getResource("espaceFournisseur.fxml"));
+        Scene ListeE = new Scene(liste);
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        window.setScene(ListeE);
+        window.show();
     }
     
 }
