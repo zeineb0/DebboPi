d3171a3720cbaa66c53c1d7d3dc4e14b19aa71fb
Bafarouk <44127908+Bafarouk@users.noreply.github.com>
2020-02-21 02:57:05 +0100

    update

---
+++ src/com/esprit/views/AjouterContratController.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.esprit.views;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+/**
+ * FXML Controller class
+ *
+ * @author Dell
+ */
+public class AjouterContratController implements Initializable {
+
+    @FXML
+    private Button addC;
+    @FXML
+    private Button updC;
+    @FXML
+    private Button delC;
+    @FXML
+    private Button readC;
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+
+    @FXML
+    private void onClickAdd(ActionEvent event) {
+    }
+
+    @FXML
+    private void onClickUpdate(ActionEvent event) {
+    }
+
+    @FXML
+    private void onClickDelete(ActionEvent event) {
+    }
+
+    @FXML
+    private void onClickRead(ActionEvent event) {
+    }
+    
+}
