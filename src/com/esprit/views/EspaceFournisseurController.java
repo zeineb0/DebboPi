1280744028d018d45a9309822740e2f2c099a0ff
fatmahammami <60627599+fatmahammami@users.noreply.github.com>
2020-02-23 12:04:50 +0100

    Update EspaceFournisseurController.java

--- src/com/esprit/views/EspaceFournisseurController.java
+++ src/com/esprit/views/EspaceFournisseurController.java
@@ -12,15 +12,19 @@ import java.net.URL;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
@@ -54,7 +58,11 @@ public class EspaceFournisseurController implements Initializable {
     ServiceEntrepot serviceEntrepot = new ServiceEntrepot();
     @FXML
     private TableColumn<Entrepot, Double> prix;
+    private Entrepot EntrepotSelectionner = new Entrepot();
 
+int id;
+    @FXML
+    private Button supp;
     //public Observablelist<Entrepot> data= FXCollections.observableArrayList();
     /**
      * Initializes the controller class.
@@ -65,7 +73,6 @@ public class EspaceFournisseurController implements Initializable {
             ArrayList<Entrepot> entrepots = (ArrayList<Entrepot>) serviceEntrepot.readAll();
                 
             datalist = FXCollections.observableArrayList(entrepots);
-            Ide.setCellValueFactory(new PropertyValueFactory<>("id_entrepot"));
             adre.setCellValueFactory(new PropertyValueFactory<>("adresse_entrepot"));
             num.setCellValueFactory(new PropertyValueFactory<>("num_fiscale"));
             quant.setCellValueFactory(new PropertyValueFactory<>("quantite_max"));
@@ -78,7 +85,45 @@ public class EspaceFournisseurController implements Initializable {
         } catch (SQLException ex) {
              System.out.println("com.esprit.views.AfficherEntrepotController.onClick()");
         }
+        table.setOnMouseClicked(event->{
+        //pour modifier un produit il faut faire deux click
+              if (event.getClickCount() == 1) {
+                  EntrepotSelectionner=table.getItems().get(table.getSelectionModel().getSelectedIndex());
+                  id= EntrepotSelectionner.getId_entrepot();
+                supp.setOnAction(new EventHandler<ActionEvent>() {
+ 
+                   
+                @Override
+                      public void handle(ActionEvent event) {
+                        try {
+                          serviceEntrepot.delete(id);
+                          try {
+            ArrayList<Entrepot> entrepots = (ArrayList<Entrepot>) serviceEntrepot.readAll();
+                
+            datalist = FXCollections.observableArrayList(entrepots);
+            adre.setCellValueFactory(new PropertyValueFactory<>("adresse_entrepot"));
+            num.setCellValueFactory(new PropertyValueFactory<>("num_fiscale"));
+            quant.setCellValueFactory(new PropertyValueFactory<>("quantite_max"));
+            etat.setCellValueFactory(new PropertyValueFactory<>("etat"));
+            prix.setCellValueFactory(new PropertyValueFactory<>("prix_location"));
+            entrep.setCellValueFactory(new PropertyValueFactory<>("entreprise"));
+            
+            table.setItems(datalist);
+      
+        } catch (SQLException ex) {
+             System.out.println("com.esprit.views.AfficherEntrepotController.onClick()");
+        }
+                      } catch (SQLException ex) {
+                            System.out.println(".handle()");                      }
+                          }
+                  
+                  
            
+                 }); 
+                  
+              }
+                    });
+                      
     
             }
 
@@ -91,16 +136,9 @@ public class EspaceFournisseurController implements Initializable {
         window.setScene(ListeE);
         window.show();
     }  
-    @FXML
-    private void onClick1(ActionEvent event) throws IOException {
-         
-        Parent liste = FXMLLoader.load(getClass().getResource("supprimerEntrepot.fxml"));
-           Scene ListeE = new Scene(liste);
-        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        window.setScene(ListeE);
-        window.show();
-        
-    }   
+ 
+      
+     
     @FXML
     private void onClick2(ActionEvent event) throws IOException {
         Parent liste = FXMLLoader.load(getClass().getResource("modifierEntrepot.fxml"));
