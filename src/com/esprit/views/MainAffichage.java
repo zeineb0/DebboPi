a0d5c23b6cfa73c4eeacba01528c0a0f4363d2f8
TaherZ <taher.zoghlami1b@gmail.com>
2020-02-27 15:42:51 +0100

    Update MainAffichage.java

--- src/com/esprit/views/MainAffichage.java
+++ src/com/esprit/views/MainAffichage.java
@@ -26,7 +26,7 @@ public class MainAffichage extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            Parent root = FXMLLoader.load(getClass().getResource("Statistiques.fxml"));     
+            Parent root = FXMLLoader.load(getClass().getResource("Affichage.fxml"));     
             Scene scene = new Scene(root);
             primaryStage.setTitle("Hello World!");
             primaryStage.setScene(scene);
